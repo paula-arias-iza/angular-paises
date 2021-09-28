a308dd3a742181289d5ba0afc017c6bc5a5b661e
Paula AI <paula.arias.iza@gmail.com>
2021-09-28 14:35:44 +0200

    Buscador de países

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
+  ]
+})
+export class SidebarComponent {
+
+  constructor() { }
+}
